ecb27fe302941dcaff4a98845d5e3093d668140b
swe-bench <>
2024-06-12 16:29:59 -0400

    Task instance HumanEval/29 with tests

---
+++ test.py
@@ -0,0 +1,13 @@
+from main import filter_by_prefix
+
+
+METADATA = {
+    'author': 'jt',
+    'dataset': 'test'
+}
+
+
+def check(candidate):
+    assert candidate([], 'john') == []
+    assert candidate(['xxx', 'asd', 'xxy', 'john doe', 'xxxAAA', 'xxx'], 'xxx') == ['xxx', 'xxxAAA', 'xxx']
+check(filter_by_prefix)
