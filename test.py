9aec341e86a5510c9c48cf3da5d2c3159e3dce8f
swe-bench <>
2024-06-12 16:30:00 -0400

    Task instance HumanEval/51 with tests

---
+++ test.py
@@ -0,0 +1,16 @@
+from main import remove_vowels
+
+
+METADATA = {}
+
+
+def check(candidate):
+    assert candidate('') == ''
+    assert candidate("abcdef\nghijklm") == 'bcdf\nghjklm'
+    assert candidate('fedcba') == 'fdcb'
+    assert candidate('eeeee') == ''
+    assert candidate('acBAA') == 'cB'
+    assert candidate('EcBOO') == 'cB'
+    assert candidate('ybcd') == 'ybcd'
+
+check(remove_vowels)
