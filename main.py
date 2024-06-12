13493ed349cc276ab9633e310b7c572eaf76d8e2
swe-bench <>
2024-06-12 16:30:07 -0400

    Task instance HumanEval/150

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
 
-def sorted_list_sum(lst):
-    """Write a function that accepts a list of strings as a parameter,
-    deletes the strings that have odd lengths from it,
-    and returns the resulted list with a sorted order,
-    The list is always a list of strings and never an array of numbers,
-    and it may contain duplicates.
-    The order of the list should be ascending by length of each word, and you
-    should return the list sorted by that rule.
-    If two words have the same length, sort the list alphabetically.
-    The function should return a list of strings in sorted order.
-    You may assume that all words will have the same length.
-    For example:
-    assert list_sort(["aa", "a", "aaa"]) => ["aa"]
-    assert list_sort(["ab", "a", "aaa", "cd"]) => ["ab", "cd"]
+def x_or_y(n, x, y):
+    """A simple program which should return the value of x if n is 
+    a prime number and should return the value of y otherwise.
+
+    Examples:
+    for x_or_y(7, 34, 12) == 34
+    for x_or_y(15, 8, 5) == 5
+    
     """
