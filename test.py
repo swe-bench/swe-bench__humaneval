3fa8c714d7a2f2787f322284e5d9cb6d7742924a
swe-bench <>
2024-06-12 16:30:00 -0400

    Task instance HumanEval/48 with tests

---
+++ test.py
@@ -0,0 +1,16 @@
+from main import is_palindrome
+
+
+METADATA = {}
+
+
+def check(candidate):
+    assert candidate('') == True
+    assert candidate('aba') == True
+    assert candidate('aaaaa') == True
+    assert candidate('zbcd') == False
+    assert candidate('xywyx') == True
+    assert candidate('xywyz') == False
+    assert candidate('xywzx') == False
+
+check(is_palindrome)
