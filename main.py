909c94fbeb59dbab140adf53db0d860fdbdaaabf
swe-bench <>
2024-06-12 16:29:58 -0400

    Task instance HumanEval/20

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from typing import List
+from typing import List, Tuple
 
 
-def sort_numbers(numbers: str) -> str:
-    """ Input is a space-delimited string of numberals from 'zero' to 'nine'.
-    Valid choices are 'zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight' and 'nine'.
-    Return the string with numbers sorted from smallest to largest
-    >>> sort_numbers('three one five')
-    'one three five'
+def find_closest_elements(numbers: List[float]) -> Tuple[float, float]:
+    """ From a supplied list of numbers (of length at least two) select and return two that are the closest to each
+    other and return them in order (smaller number, larger number).
+    >>> find_closest_elements([1.0, 2.0, 3.0, 4.0, 5.0, 2.2])
+    (2.0, 2.2)
+    >>> find_closest_elements([1.0, 2.0, 3.0, 4.0, 5.0, 2.0])
+    (2.0, 2.0)
     """
