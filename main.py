d41364a07f830450ff7affc81779a0f6b2874499
swe-bench <>
2024-06-12 16:30:06 -0400

    Task instance HumanEval/145

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 
-def simplify(x, n):
-    """Your task is to implement a function that will simplify the expression
-    x * n. The function returns True if x * n evaluates to a whole number and False
-    otherwise. Both x and n, are string representation of a fraction, and have the following format,
-    <numerator>/<denominator> where both numerator and denominator are positive whole numbers.
+def order_by_points(nums):
+    """
+    Write a function which sorts the given list of integers
+    in ascending order according to the sum of their digits.
+    Note: if there are several items with similar sum of their digits,
+    order them based on their index in original list.
 
-    You can assume that x, and n are valid fractions, and do not have zero as denominator.
-
-    simplify("1/5", "5/1") = True
-    simplify("1/6", "2/1") = False
-    simplify("7/10", "10/2") = False
+    For example:
+    >>> order_by_points([1, 11, -1, -11, -12]) == [-1, -11, 1, -12, 11]
+    >>> order_by_points([]) == []
     """
