949f5cd6d5410e0a346d4f4412b25995ae6fbc16
swe-bench <>
2024-06-12 16:29:58 -0400

    Task instance HumanEval/25

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
+from typing import List
 
 
-def largest_divisor(n: int) -> int:
-    """ For a given number n, find the largest number that divides n evenly, smaller than n
-    >>> largest_divisor(15)
-    5
+def factorize(n: int) -> List[int]:
+    """ Return list of prime factors of given integer in the order from smallest to largest.
+    Each of the factors should be listed number of times corresponding to how many times it appeares in factorization.
+    Input number should be equal to the product of all factors
+    >>> factorize(8)
+    [2, 2, 2]
+    >>> factorize(25)
+    [5, 5]
+    >>> factorize(70)
+    [2, 5, 7]
     """
