c2807485ffa3a611aeb2ef05b80ed1b4ea65910b
swe-bench <>
2024-06-12 16:30:06 -0400

    Task instance HumanEval/138

--- main.py
+++ main.py
@@ -1,13 +1,8 @@
 
-def compare_one(a, b):
-    """
-    Create a function that takes integers, floats, or strings representing
-    real numbers, and returns the larger variable in its given variable type.
-    Return None if the values are equal.
-    Note: If a real number is represented as a string, the floating point might be . or ,
-
-    compare_one(1, 2.5) ➞ 2.5
-    compare_one(1, "2,3") ➞ "2,3"
-    compare_one("5,1", "6") ➞ "6"
-    compare_one("1", 1) ➞ None
+def is_equal_to_sum_even(n):
+    """Evaluate whether the given number n can be written as the sum of exactly 4 positive even numbers
+    Example
+    is_equal_to_sum_even(4) == False
+    is_equal_to_sum_even(6) == False
+    is_equal_to_sum_even(8) == True
     """
