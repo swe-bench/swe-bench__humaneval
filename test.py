11d8239fff263064f2cc4724701cf404b29515b3
swe-bench <>
2024-06-12 16:29:58 -0400

    Task instance HumanEval/24 with tests

---
+++ test.py
@@ -0,0 +1,16 @@
+from main import largest_divisor
+
+
+METADATA = {
+    'author': 'jt',
+    'dataset': 'test'
+}
+
+
+def check(candidate):
+    assert candidate(3) == 1
+    assert candidate(7) == 1
+    assert candidate(10) == 5
+    assert candidate(100) == 50
+    assert candidate(49) == 7
+check(largest_divisor)
