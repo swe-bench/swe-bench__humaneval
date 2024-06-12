cc04455ff580f3ca97211a027292434891d895ef
swe-bench <>
2024-06-12 16:29:59 -0400

    Task instance HumanEval/41 with tests

---
+++ test.py
@@ -0,0 +1,14 @@
+from main import car_race_collision
+
+
+METADATA = {}
+
+
+def check(candidate):
+    assert candidate(2) == 4
+    assert candidate(3) == 9
+    assert candidate(4) == 16
+    assert candidate(8) == 64
+    assert candidate(10) == 100
+
+check(car_race_collision)
