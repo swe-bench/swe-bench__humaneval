23b04ef03765bf7f25fca8f975eb3a740da3e8ae
swe-bench <>
2024-06-12 16:30:03 -0400

    Task instance HumanEval/96

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 
-def check_dict_case(dict):
-    """
-    Given a dictionary, return True if all keys are strings in lower 
-    case or all keys are strings in upper case, else return False.
-    The function should return False is the given dictionary is empty.
-    Examples:
-    check_dict_case({"a":"apple", "b":"banana"}) should return True.
-    check_dict_case({"a":"apple", "A":"banana", "B":"banana"}) should return False.
-    check_dict_case({"a":"apple", 8:"banana", "a":"apple"}) should return False.
-    check_dict_case({"Name":"John", "Age":"36", "City":"Houston"}) should return False.
-    check_dict_case({"STATE":"NC", "ZIP":"12345" }) should return True.
+def count_up_to(n):
+    """Implement a function that takes an non-negative integer and returns an array of the first n
+    integers that are prime numbers and less than n.
+    for example:
+    count_up_to(5) => [2,3]
+    count_up_to(11) => [2,3,5,7]
+    count_up_to(0) => []
+    count_up_to(20) => [2,3,5,7,11,13,17,19]
+    count_up_to(1) => []
+    count_up_to(18) => [2,3,5,7,11,13,17]
     """
