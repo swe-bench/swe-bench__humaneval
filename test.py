1c82d60bd787830a42e896ed4e077fbb76e6f8ba
swe-bench <>
2024-06-12 16:30:05 -0400

    Task instance HumanEval/125 with tests

---
+++ test.py
@@ -0,0 +1,12 @@
+from main import split_words
+def check(candidate):
+
+    assert candidate("Hello world!") == ["Hello","world!"]
+    assert candidate("Hello,world!") == ["Hello","world!"]
+    assert candidate("Hello world,!") == ["Hello","world,!"]
+    assert candidate("Hello,Hello,world !") == ["Hello,Hello,world","!"]
+    assert candidate("abcdef") == 3
+    assert candidate("aaabb") == 2
+    assert candidate("aaaBb") == 1
+    assert candidate("") == 0
+check(split_words)
