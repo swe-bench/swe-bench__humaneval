322878a6dc86b6ead77d5e61f9cc2b64ab421888
swe-bench <>
2024-06-12 16:29:59 -0400

    Task instance HumanEval/42

--- main.py
+++ main.py
@@ -1,14 +1,9 @@
 
 
-def car_race_collision(n: int):
-    """
-    Imagine a road that's a perfectly straight infinitely long line.
-    n cars are driving left to right;  simultaneously, a different set of n cars
-    are driving right to left.   The two sets of cars start out being very far from
-    each other.  All cars move in the same speed.  Two cars are said to collide
-    when a car that's moving left to right hits a car that's moving right to left.
-    However, the cars are infinitely sturdy and strong; as a result, they continue moving
-    in their trajectory as if they did not collide.
-
-    This function outputs the number of such collisions.
+def incr_list(l: list):
+    """Return list with elements incremented by 1.
+    >>> incr_list([1, 2, 3])
+    [2, 3, 4]
+    >>> incr_list([5, 3, 5, 2, 3, 3, 9, 0, 123])
+    [6, 4, 6, 3, 4, 4, 10, 1, 124]
     """
