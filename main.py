9d17590f58a700a9c5ac37bfea5040e8e2235455
swe-bench <>
2024-06-12 16:29:57 -0400

    Task instance HumanEval/4

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from typing import List
 
 
-def below_zero(operations: List[int]) -> bool:
-    """ You're given a list of deposit and withdrawal operations on a bank account that starts with
-    zero balance. Your task is to detect if at any point the balance of account fallls below zero, and
-    at that point function should return True. Otherwise it should return False.
-    >>> below_zero([1, 2, 3])
-    False
-    >>> below_zero([1, 2, -4, 5])
-    True
+def mean_absolute_deviation(numbers: List[float]) -> float:
+    """ For a given list of input numbers, calculate Mean Absolute Deviation
+    around the mean of this dataset.
+    Mean Absolute Deviation is the average absolute difference between each
+    element and a centerpoint (mean in this case):
+    MAD = average | x - x_mean |
+    >>> mean_absolute_deviation([1.0, 2.0, 3.0, 4.0])
+    1.0
     """
