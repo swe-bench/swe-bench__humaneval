1c61da271d6017d96e0c618dfc64cb32efdeffbd
swe-bench <>
2024-06-12 16:30:04 -0400

    Task instance HumanEval/112 with tests

---
+++ test.py
@@ -0,0 +1,13 @@
+from main import reverse_delete
+def check(candidate):
+
+    assert candidate("abcde","ae") == ('bcd',False)
+    assert candidate("abcdef", "b") == ('acdef',False)
+    assert candidate("abcdedcba","ab") == ('cdedc',True)
+    assert candidate("dwik","w") == ('dik',False)
+    assert candidate("a","a") == ('',True)
+    assert candidate("abcdedcba","") == ('abcdedcba',True)
+    assert candidate("abcdedcba","v") == ('abcdedcba',True)
+    assert candidate("vabba","v") == ('abba',True)
+    assert candidate("mamma", "mia") == ("", True)
+check(reverse_delete)
