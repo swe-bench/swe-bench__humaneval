856e7ad36322d96568e914c6f88b9a06e646907c
swe-bench <>
2024-06-12 16:29:58 -0400

    Task instance HumanEval/26

--- main.py
+++ main.py
@@ -1,14 +1,9 @@
 from typing import List
 
 
-def factorize(n: int) -> List[int]:
-    """ Return list of prime factors of given integer in the order from smallest to largest.
-    Each of the factors should be listed number of times corresponding to how many times it appeares in factorization.
-    Input number should be equal to the product of all factors
-    >>> factorize(8)
-    [2, 2, 2]
-    >>> factorize(25)
-    [5, 5]
-    >>> factorize(70)
-    [2, 5, 7]
+def remove_duplicates(numbers: List[int]) -> List[int]:
+    """ From a list of integers, remove all elements that occur more than once.
+    Keep order of elements left the same as in the input.
+    >>> remove_duplicates([1, 2, 3, 2, 4])
+    [1, 3, 4]
     """
