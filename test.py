636f4cb23e0b0e7e2f60ebffbd5b620c9f12b936
swe-bench <>
2024-06-12 16:30:00 -0400

    Task instance HumanEval/54 with tests

---
+++ test.py
@@ -0,0 +1,16 @@
+from main import same_chars
+
+
+METADATA = {}
+
+
+def check(candidate):
+    assert candidate('eabcdzzzz', 'dddzzzzzzzddeddabc') == True
+    assert candidate('abcd', 'dddddddabc') == True
+    assert candidate('dddddddabc', 'abcd') == True
+    assert candidate('eabcd', 'dddddddabc') == False
+    assert candidate('abcd', 'dddddddabcf') == False
+    assert candidate('eabcdzzzz', 'dddzzzzzzzddddabc') == False
+    assert candidate('aabb', 'aaccc') == False
+
+check(same_chars)
