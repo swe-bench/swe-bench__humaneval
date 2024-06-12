d14ac4123d95e28fdf82b8537e5a537577ad91f9
swe-bench <>
2024-06-12 16:30:01 -0400

    Task instance HumanEval/62

--- main.py
+++ main.py
@@ -1,15 +1,11 @@
 
 
-def correct_bracketing(brackets: str):
-    """ brackets is a string of "(" and ")".
-    return True if every opening bracket has a corresponding closing bracket.
-
-    >>> correct_bracketing("(")
-    False
-    >>> correct_bracketing("()")
-    True
-    >>> correct_bracketing("(()())")
-    True
-    >>> correct_bracketing(")(()")
-    False
+def derivative(xs: list):
+    """ xs represent coefficients of a polynomial.
+    xs[0] + xs[1] * x + xs[2] * x^2 + ....
+     Return derivative of this polynomial in the same form.
+    >>> derivative([3, 1, 2, 4, 5])
+    [1, 4, 12, 20]
+    >>> derivative([1, 2, 3])
+    [2, 6]
     """
