38465a2c76e81b1a52c0c8e377b74a43f91d2b14
swe-bench <>
2024-06-12 16:30:00 -0400

    Task instance HumanEval/45

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
 
 
-def change_base(x: int, base: int):
-    """Change numerical base of input number x to base.
-    return string representation after the conversion.
-    base numbers are less than 10.
-    >>> change_base(8, 3)
-    '22'
-    >>> change_base(8, 2)
-    '1000'
-    >>> change_base(7, 2)
-    '111'
+def triangle_area(a, h):
+    """Given length of a side and high return area for a triangle.
+    >>> triangle_area(5, 3)
+    7.5
     """
