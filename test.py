46a7c5f5d69b8f1d90de8cdfd72cfd0ec22f2d5e
swe-bench <>
2024-06-12 16:30:00 -0400

    Task instance HumanEval/46 with tests

---
+++ test.py
@@ -0,0 +1,13 @@
+from main import fib4
+
+
+METADATA = {}
+
+
+def check(candidate):
+    assert candidate(5) == 4
+    assert candidate(8) == 28
+    assert candidate(10) == 104
+    assert candidate(12) == 386
+
+check(fib4)
