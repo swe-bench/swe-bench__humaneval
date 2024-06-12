9cd10835d8520973fe4381b9d63819c55e04cd16
swe-bench <>
2024-06-12 16:30:03 -0400

    Task instance HumanEval/92

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 
-def is_bored(S):
-    """
-    You'll be given a string of words, and your task is to count the number
-    of boredoms. A boredom is a sentence that starts with the word "I".
-    Sentences are delimited by '.', '?' or '!'.
-   
-    For example:
-    >>> is_bored("Hello world")
-    0
-    >>> is_bored("The sky is blue. The sun is shining. I love this weather")
-    1
-    """
+def any_int(x, y, z):
+    '''
+    Create a function that takes 3 numbers.
+    Returns true if one of the numbers is equal to the sum of the other two, and all numbers are integers.
+    Returns false in any other cases.
+    
+    Examples
+    any_int(5, 2, 7) ➞ True
+    
+    any_int(3, 2, 2) ➞ False
+
+    any_int(3, -2, 1) ➞ True
+    
+    any_int(3.6, -2.2, 2) ➞ False
+  
+
+    
+    '''
