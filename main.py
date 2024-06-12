ea7ff9d0ab2ded647058bc2713ce450ddd0331c6
swe-bench <>
2024-06-12 16:30:05 -0400

    Task instance HumanEval/125

--- main.py
+++ main.py
@@ -1,21 +1,11 @@
 
-def valid_date(date):
-    """You have to write a function which validates a given date string and
-    returns True if the date is valid otherwise False.
-    The date is valid if all of the following rules are satisfied:
-    1. The date string is not empty.
-    2. The number of days is not less than 1 or higher than 31 days for months 1,3,5,7,8,10,12. And the number of days is not less than 1 or higher than 30 days for months 4,6,9,11. And, the number of days is not less than 1 or higher than 29 for the month 2.
-    3. The months should not be less than 1 or higher than 12.
-    4. The date should be in the format: mm-dd-yyyy
-
-    for example: 
-    valid_date('03-11-2000') => True
-
-    valid_date('15-01-2012') => False
-
-    valid_date('04-0-2040') => False
-
-    valid_date('06-04-2020') => True
-
-    valid_date('06/04/2020') => False
-    """
+def split_words(txt):
+    '''
+    Given a string of words, return a list of words split on whitespace, if no whitespaces exists in the text you
+    should split on commas ',' if no commas exists you should return the number of lower-case letters with odd order in the
+    alphabet, ord('a') = 0, ord('b') = 1, ... ord('z') = 25
+    Examples
+    split_words("Hello world!") ➞ ["Hello", "world!"]
+    split_words("Hello,world!") ➞ ["Hello", "world!"]
+    split_words("abcdef") == 3 
+    '''
