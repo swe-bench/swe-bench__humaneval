ef31a643288077c2e8316e8148ed93ec0140c7cf
swe-bench <>
2024-06-12 16:30:02 -0400

    Task instance HumanEval/76

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 
-def is_multiply_prime(a):
-    """Write a function that returns true if the given number is the multiplication of 3 prime numbers
-    and false otherwise.
-    Knowing that (a) is less then 100. 
-    Example:
-    is_multiply_prime(30) == True
-    30 = 2 * 3 * 5
+def is_simple_power(x, n):
+    """Your task is to write a function that returns true if a number x is a simple
+    power of n and false in other cases.
+    x is a simple power of n if n**int=x
+    For example:
+    is_simple_power(1, 4) => true
+    is_simple_power(2, 2) => true
+    is_simple_power(8, 2) => true
+    is_simple_power(3, 2) => false
+    is_simple_power(3, 1) => false
+    is_simple_power(5, 3) => false
     """
