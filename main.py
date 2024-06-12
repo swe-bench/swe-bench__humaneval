4043958b48374cb1b744e78b135fcf35b39869ed
swe-bench <>
2024-06-12 16:30:05 -0400

    Task instance HumanEval/121

--- main.py
+++ main.py
@@ -1,26 +1,10 @@
 
-def maximum(arr, k):
-    """
-    Given an array arr of integers and a positive integer k, return a sorted list 
-    of length k with the maximum k numbers in arr.
+def solution(lst):
+    """Given a non-empty list of integers, return the sum of all of the odd elements that are in even positions.
+    
 
-    Example 1:
-
-        Input: arr = [-3, -4, 5], k = 3
-        Output: [-4, -3, 5]
-
-    Example 2:
-
-        Input: arr = [4, -4, 4], k = 2
-        Output: [4, 4]
-
-    Example 3:
-
-        Input: arr = [-3, 2, 1, 2, -1, -2, 1], k = 1
-        Output: [2]
-
-    Note:
-        1. The length of the array will be in the range of [1, 1000].
-        2. The elements in the array will be in the range of [-1000, 1000].
-        3. 0 <= k <= len(arr)
+    Examples
+    solution([5, 8, 7, 1]) ==> 12
+    solution([3, 3, 3, 3, 3]) ==> 9
+    solution([30, 13, 24, 321]) ==>0
     """
