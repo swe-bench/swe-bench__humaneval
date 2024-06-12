c2fcf6fc3b2de6592cfb9f3e4398dd370752159c
swe-bench <>
2024-06-12 16:29:59 -0400

    Task instance HumanEval/27 with tests

---
+++ test.py
@@ -0,0 +1,14 @@
+from main import flip_case
+
+
+METADATA = {
+    'author': 'jt',
+    'dataset': 'test'
+}
+
+
+def check(candidate):
+    assert candidate('') == ''
+    assert candidate('Hello!') == 'hELLO!'
+    assert candidate('These violent delights have violent ends') == 'tHESE VIOLENT DELIGHTS HAVE VIOLENT ENDS'
+check(flip_case)
