b4269a5ae8af00adcda9b962ddefb08ff3485da8
swe-bench <>
2024-06-12 16:30:07 -0400

    Task instance HumanEval/149 with tests

---
+++ test.py
@@ -0,0 +1,15 @@
+from main import sorted_list_sum
+def check(candidate):
+
+    # Check some simple cases
+    assert candidate(["aa", "a", "aaa"]) == ["aa"]
+    assert candidate(["school", "AI", "asdf", "b"]) == ["AI", "asdf", "school"]
+    assert candidate(["d", "b", "c", "a"]) == []
+    assert candidate(["d", "dcba", "abcd", "a"]) == ["abcd", "dcba"]
+
+    # Check some edge cases that are easy to work out by hand.
+    assert candidate(["AI", "ai", "au"]) == ["AI", "ai", "au"]
+    assert candidate(["a", "b", "b", "c", "c", "a"]) == []
+    assert candidate(['aaaa', 'bbbb', 'dd', 'cc']) == ["cc", "dd", "aaaa", "bbbb"]
+
+check(sorted_list_sum)
