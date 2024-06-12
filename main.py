9d21c208a241731362abfb6686317cecec6523c4
swe-bench <>
2024-06-12 16:30:01 -0400

    Task instance HumanEval/67

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 
-def digitSum(s):
-    """Task
-    Write a function that takes a string as input and returns the sum of the upper characters only'
-    ASCII codes.
-
-    Examples:
-        digitSum("") => 0
-        digitSum("abAB") => 131
-        digitSum("abcCd") => 67
-        digitSum("helloE") => 69
-        digitSum("woArBld") => 131
-        digitSum("aAaaaXa") => 153
+def fruit_distribution(s,n):
+    """
+    In this task, you will be given a string that represents a number of apples and oranges 
+    that are distributed in a basket of fruit this basket contains 
+    apples, oranges, and mango fruits. Given the string that represents the total number of 
+    the oranges and apples and an integer that represent the total number of the fruits 
+    in the basket return the number of the mango fruits in the basket.
+    for examble:
+    fruit_distribution("5 apples and 6 oranges", 19) ->19 - 5 - 6 = 8
+    fruit_distribution("0 apples and 1 oranges",3) -> 3 - 0 - 1 = 2
+    fruit_distribution("2 apples and 3 oranges", 100) -> 100 - 2 - 3 = 95
+    fruit_distribution("100 apples and 1 oranges",120) -> 120 - 100 - 1 = 19
     """
