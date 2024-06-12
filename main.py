75f00ac4c53db9899bc97113d73105788ddbbcb4
swe-bench <>
2024-06-12 16:29:58 -0400

    Task instance HumanEval/24

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 
 
-def strlen(string: str) -> int:
-    """ Return length of given string
-    >>> strlen('')
-    0
-    >>> strlen('abc')
-    3
+def largest_divisor(n: int) -> int:
+    """ For a given number n, find the largest number that divides n evenly, smaller than n
+    >>> largest_divisor(15)
+    5
     """
