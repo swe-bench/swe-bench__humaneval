99fb2fc75dfe3d82598e73c2d0af69eb8ba358b2
swe-bench <>
2024-06-12 16:29:57 -0400

    Task instance HumanEval/12 with tests

---
+++ test.py
@@ -0,0 +1,14 @@
+from main import longest
+
+
+METADATA = {
+    'author': 'jt',
+    'dataset': 'test'
+}
+
+
+def check(candidate):
+    assert candidate([]) == None
+    assert candidate(['x', 'y', 'z']) == 'x'
+    assert candidate(['x', 'yyy', 'zzzz', 'www', 'kkkk', 'abc']) == 'zzzz'
+check(longest)
