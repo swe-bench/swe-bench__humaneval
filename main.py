e6a6eeaa84ce733b2e2786c3cc46138e3046a0bc
swe-bench <>
2024-06-12 16:30:03 -0400

    Task instance HumanEval/91

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 
-def next_smallest(lst):
+def is_bored(S):
     """
-    You are given a list of integers.
-    Write a function next_smallest() that returns the 2nd smallest element of the list.
-    Return None if there is no such element.
-    
-    next_smallest([1, 2, 3, 4, 5]) == 2
-    next_smallest([5, 1, 4, 3, 2]) == 2
-    next_smallest([]) == None
-    next_smallest([1, 1]) == None
+    You'll be given a string of words, and your task is to count the number
+    of boredoms. A boredom is a sentence that starts with the word "I".
+    Sentences are delimited by '.', '?' or '!'.
+   
+    For example:
+    >>> is_bored("Hello world")
+    0
+    >>> is_bored("The sky is blue. The sun is shining. I love this weather")
+    1
     """
