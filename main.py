6eae9dc0efc14305a803fac325adfea9c7373147
swe-bench <>
2024-06-12 16:29:58 -0400

    Task instance HumanEval/22

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from typing import List
+from typing import List, Any
 
 
-def rescale_to_unit(numbers: List[float]) -> List[float]:
-    """ Given list of numbers (of at least two elements), apply a linear transform to that list,
-    such that the smallest number will become 0 and the largest will become 1
-    >>> rescale_to_unit([1.0, 2.0, 3.0, 4.0, 5.0])
-    [0.0, 0.25, 0.5, 0.75, 1.0]
+def filter_integers(values: List[Any]) -> List[int]:
+    """ Filter given list of any python values only for integers
+    >>> filter_integers(['a', 3.14, 5])
+    [5]
+    >>> filter_integers([1, 2, 3, 'abc', {}, []])
+    [1, 2, 3]
     """
