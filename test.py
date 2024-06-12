3a2c23763f94e73fb5d6c8d045a336ae108df19a
swe-bench <>
2024-06-12 16:30:01 -0400

    Task instance HumanEval/61 with tests

---
+++ test.py
@@ -0,0 +1,21 @@
+from main import correct_bracketing
+
+
+METADATA = {}
+
+
+def check(candidate):
+    assert candidate("()")
+    assert candidate("(()())")
+    assert candidate("()()(()())()")
+    assert candidate("()()((()()())())(()()(()))")
+    assert not candidate("((()())))")
+    assert not candidate(")(()")
+    assert not candidate("(")
+    assert not candidate("((((")
+    assert not candidate(")")
+    assert not candidate("(()")
+    assert not candidate("()()(()())())(()")
+    assert not candidate("()()(()())()))()")
+
+check(correct_bracketing)
