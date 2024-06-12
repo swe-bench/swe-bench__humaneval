c6cdb7e448e685549f82e67d6f460951a9f6168d
swe-bench <>
2024-06-12 16:29:57 -0400

    Task instance HumanEval/8 with tests

---
+++ test.py
@@ -0,0 +1,16 @@
+from main import sum_product
+
+
+METADATA = {
+    'author': 'jt',
+    'dataset': 'test'
+}
+
+
+def check(candidate):
+    assert candidate([]) == (0, 1)
+    assert candidate([1, 1, 1]) == (3, 1)
+    assert candidate([100, 0]) == (100, 0)
+    assert candidate([3, 5, 7]) == (3 + 5 + 7, 3 * 5 * 7)
+    assert candidate([10]) == (10, 10)
+check(sum_product)
