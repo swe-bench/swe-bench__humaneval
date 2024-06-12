d4614b995c810b41c82a276fa675ef0ff72bca39
swe-bench <>
2024-06-12 16:29:58 -0400

    Task instance HumanEval/27

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-from typing import List
 
 
-def remove_duplicates(numbers: List[int]) -> List[int]:
-    """ From a list of integers, remove all elements that occur more than once.
-    Keep order of elements left the same as in the input.
-    >>> remove_duplicates([1, 2, 3, 2, 4])
-    [1, 3, 4]
+def flip_case(string: str) -> str:
+    """ For a given string, flip lowercase characters to uppercase and uppercase to lowercase.
+    >>> flip_case('Hello')
+    'hELLO'
     """
