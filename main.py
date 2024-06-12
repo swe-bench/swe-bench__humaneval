9056cb1b346d8c5b48f645a047f8e4985cf2b9c2
swe-bench <>
2024-06-12 16:29:58 -0400

    Task instance HumanEval/23

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-from typing import List, Any
 
 
-def filter_integers(values: List[Any]) -> List[int]:
-    """ Filter given list of any python values only for integers
-    >>> filter_integers(['a', 3.14, 5])
-    [5]
-    >>> filter_integers([1, 2, 3, 'abc', {}, []])
-    [1, 2, 3]
+def strlen(string: str) -> int:
+    """ Return length of given string
+    >>> strlen('')
+    0
+    >>> strlen('abc')
+    3
     """
