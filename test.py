f2fb2ee5ef9e50a87ad97b64f61ee2fb789e0d8f
swe-bench <>
2024-06-12 16:29:58 -0400

    Task instance HumanEval/14 with tests

---
+++ test.py
@@ -0,0 +1,14 @@
+from main import all_prefixes
+
+
+METADATA = {
+    'author': 'jt',
+    'dataset': 'test'
+}
+
+
+def check(candidate):
+    assert candidate('') == []
+    assert candidate('asdfgh') == ['a', 'as', 'asd', 'asdf', 'asdfg', 'asdfgh']
+    assert candidate('WWW') == ['W', 'WW', 'WWW']
+check(all_prefixes)
