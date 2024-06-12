04ef04b17eedb5915c6969e6dbf0eb8f879a66a4
swe-bench <>
2024-06-12 16:29:59 -0400

    Task instance HumanEval/39

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
 
 
-def encode_cyclic(s: str):
+def prime_fib(n: int):
     """
-    returns encoded string by cycling groups of three characters.
-    """
-    # split string to groups. Each of length 3.
-    groups = [s[(3 * i):min((3 * i + 3), len(s))] for i in range((len(s) + 2) // 3)]
-    # cycle elements in each group. Unless group has fewer elements than 3.
-    groups = [(group[1:] + group[0]) if len(group) == 3 else group for group in groups]
-    return "".join(groups)
-
-
-def decode_cyclic(s: str):
-    """
-    takes as input string encoded with encode_cyclic function. Returns decoded string.
+    prime_fib returns n-th number that is a Fibonacci number and it's also prime.
+    >>> prime_fib(1)
+    2
+    >>> prime_fib(2)
+    3
+    >>> prime_fib(3)
+    5
+    >>> prime_fib(4)
+    13
+    >>> prime_fib(5)
+    89
     """
