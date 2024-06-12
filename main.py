49c7a86d71a65df8e9286b104155d8eaeffa19b2
swe-bench <>
2024-06-12 16:30:03 -0400

    Task instance HumanEval/95

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 
-
-def skjkasdkd(lst):
-    """You are given a list of integers.
-    You need to find the largest prime value and return the sum of its digits.
-
+def check_dict_case(dict):
+    """
+    Given a dictionary, return True if all keys are strings in lower 
+    case or all keys are strings in upper case, else return False.
+    The function should return False is the given dictionary is empty.
     Examples:
-    For lst = [0,3,2,1,3,5,7,4,5,5,5,2,181,32,4,32,3,2,32,324,4,3] the output should be 10
-    For lst = [1,0,1,8,2,4597,2,1,3,40,1,2,1,2,4,2,5,1] the output should be 25
-    For lst = [1,3,1,32,5107,34,83278,109,163,23,2323,32,30,1,9,3] the output should be 13
-    For lst = [0,724,32,71,99,32,6,0,5,91,83,0,5,6] the output should be 11
-    For lst = [0,81,12,3,1,21] the output should be 3
-    For lst = [0,8,1,2,1,7] the output should be 7
+    check_dict_case({"a":"apple", "b":"banana"}) should return True.
+    check_dict_case({"a":"apple", "A":"banana", "B":"banana"}) should return False.
+    check_dict_case({"a":"apple", 8:"banana", "a":"apple"}) should return False.
+    check_dict_case({"Name":"John", "Age":"36", "City":"Houston"}) should return False.
+    check_dict_case({"STATE":"NC", "ZIP":"12345" }) should return True.
     """
