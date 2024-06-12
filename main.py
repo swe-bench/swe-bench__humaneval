c8bdeebe411a3f868007b5c79b0de0e3a7a47d35
swe-bench <>
2024-06-12 16:29:57 -0400

    Task instance HumanEval/13

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
-from typing import List, Optional
 
 
-def longest(strings: List[str]) -> Optional[str]:
-    """ Out of list of strings, return the longest one. Return the first one in case of multiple
-    strings of the same length. Return None in case the input list is empty.
-    >>> longest([])
-
-    >>> longest(['a', 'b', 'c'])
-    'a'
-    >>> longest(['a', 'bb', 'ccc'])
-    'ccc'
+def greatest_common_divisor(a: int, b: int) -> int:
+    """ Return a greatest common divisor of two integers a and b
+    >>> greatest_common_divisor(3, 5)
+    1
+    >>> greatest_common_divisor(25, 15)
+    5
     """
