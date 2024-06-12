644c4b2cd00ecf806376afeb780ccfd276ffc5e5
swe-bench <>
2024-06-12 16:30:06 -0400

    Task instance HumanEval/144

--- main.py
+++ main.py
@@ -1,21 +1,13 @@
 
-def words_in_sentence(sentence):
-    """
-    You are given a string representing a sentence,
-    the sentence contains some words separated by a space,
-    and you have to return a string that contains the words from the original sentence,
-    whose lengths are prime numbers,
-    the order of the words in the new string should be the same as the original one.
+def simplify(x, n):
+    """Your task is to implement a function that will simplify the expression
+    x * n. The function returns True if x * n evaluates to a whole number and False
+    otherwise. Both x and n, are string representation of a fraction, and have the following format,
+    <numerator>/<denominator> where both numerator and denominator are positive whole numbers.
 
-    Example 1:
-        Input: sentence = "This is a test"
-        Output: "is"
+    You can assume that x, and n are valid fractions, and do not have zero as denominator.
 
-    Example 2:
-        Input: sentence = "lets go for swimming"
-        Output: "go for"
-
-    Constraints:
-        * 1 <= len(sentence) <= 100
-        * sentence contains only letters
+    simplify("1/5", "5/1") = True
+    simplify("1/6", "2/1") = False
+    simplify("7/10", "10/2") = False
     """
