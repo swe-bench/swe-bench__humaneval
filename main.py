dce846f7e91a2c1f8b8b575fb434df71c701ed13
swe-bench <>
2024-06-12 16:30:02 -0400

    Task instance HumanEval/80

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 
-def decimal_to_binary(decimal):
-    """You will be given a number in decimal form and your task is to convert it to
-    binary format. The function should return a string, with each character representing a binary
-    number. Each character in the string will be '0' or '1'.
-
-    There will be an extra couple of characters 'db' at the beginning and at the end of the string.
-    The extra characters are there to help with the format.
-
-    Examples:
-    decimal_to_binary(15)   # returns "db1111db"
-    decimal_to_binary(32)   # returns "db100000db"
+def is_happy(s):
+    """You are given a string s.
+    Your task is to check if the string is happy or not.
+    A string is happy if its length is at least 3 and every 3 consecutive letters are distinct
+    For example:
+    is_happy(a) => False
+    is_happy(aa) => False
+    is_happy(abcd) => True
+    is_happy(aabb) => False
+    is_happy(adb) => True
+    is_happy(xyy) => False
     """
