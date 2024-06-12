593afcedc05b06652699bfdbd1773b32962456f3
swe-bench <>
2024-06-12 16:29:59 -0400

    Task instance HumanEval/35 with tests

---
+++ test.py
@@ -0,0 +1,10 @@
+from main import max_element
+
+
+METADATA = {}
+
+
+def check(candidate):
+    assert candidate([1, 2, 3]) == 3
+    assert candidate([5, 3, -5, 2, -3, 3, 9, 0, 124, 1, -10]) == 124
+check(max_element)
