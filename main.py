9521ac58eb4fed9de4cda3b27bb475807c9fa1a8
swe-bench <>
2024-06-12 16:30:02 -0400

    Task instance HumanEval/86

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 
-def add(lst):
-    """Given a non-empty list of integers lst. add the even elements that are at odd indices..
+def anti_shuffle(s):
+    """
+    Write a function that takes a string and returns an ordered version of it.
+    Ordered version of string, is a string where all words (separated by space)
+    are replaced by a new word where all the characters arranged in
+    ascending order based on ascii value.
+    Note: You should keep the order of words and blank spaces in the sentence.
 
-
-    Examples:
-        add([4, 2, 6, 7]) ==> 2 
+    For example:
+    anti_shuffle('Hi') returns 'Hi'
+    anti_shuffle('hello') returns 'ehllo'
+    anti_shuffle('Hello World!!!') returns 'Hello !!!Wdlor'
     """
