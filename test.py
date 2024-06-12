56ad701790d0d093131e2399d83a476c378c39e2
swe-bench <>
2024-06-12 16:30:01 -0400

    Task instance HumanEval/59 with tests

---
+++ test.py
@@ -0,0 +1,14 @@
+from main import largest_prime_factor
+
+
+METADATA = {}
+
+
+def check(candidate):
+    assert candidate(15) == 5
+    assert candidate(27) == 3
+    assert candidate(63) == 7
+    assert candidate(330) == 11
+    assert candidate(13195) == 29
+
+check(largest_prime_factor)
