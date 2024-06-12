1be5eb780a44bc2a59294c2acf4aa5f20e1adc73
swe-bench <>
2024-06-12 16:29:58 -0400

    Task instance HumanEval/19

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
+from typing import List
 
 
-def how_many_times(string: str, substring: str) -> int:
-    """ Find how many times a given substring can be found in the original string. Count overlaping cases.
-    >>> how_many_times('', 'a')
-    0
-    >>> how_many_times('aaa', 'a')
-    3
-    >>> how_many_times('aaaa', 'aa')
-    3
+def sort_numbers(numbers: str) -> str:
+    """ Input is a space-delimited string of numberals from 'zero' to 'nine'.
+    Valid choices are 'zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight' and 'nine'.
+    Return the string with numbers sorted from smallest to largest
+    >>> sort_numbers('three one five')
+    'one three five'
     """
