0ef6371ee2b8942f7208d9286faa432f149952f9
swe-bench <>
2024-06-12 16:30:00 -0400

    Task instance HumanEval/47 with tests

---
+++ test.py
@@ -0,0 +1,14 @@
+from main import median
+
+
+METADATA = {}
+
+
+def check(candidate):
+    assert candidate([3, 1, 2, 4, 5]) == 3
+    assert candidate([-10, 4, 6, 1000, 10, 20]) == 8.0
+    assert candidate([5]) == 5
+    assert candidate([6, 5]) == 5.5
+    assert candidate([8, 1, 3, 9, 9, 2, 7]) == 7 
+
+check(median)
