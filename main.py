5d6f07939c1142d172ef29b062c12ca718e87b52
swe-bench <>
2024-06-12 16:30:02 -0400

    Task instance HumanEval/83

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 
-def prime_length(string):
-    """Write a function that takes a string and returns True if the string
-    length is a prime number or False otherwise
-    Examples
-    prime_length('Hello') == True
-    prime_length('abcdcba') == True
-    prime_length('kittens') == True
-    prime_length('orange') == False
+def starts_one_ends(n):
+    """
+    Given a positive integer n, return the count of the numbers of n-digit
+    positive integers that start or end with 1.
     """
