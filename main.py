3801cc370937ab50d8fbfb62beb8fd18c0d01410
swe-bench <>
2024-06-12 16:30:03 -0400

    Task instance HumanEval/101

--- main.py
+++ main.py
@@ -1,15 +1,10 @@
 
-def make_a_pile(n):
+def words_string(s):
     """
-    Given a positive integer n, you have to make a pile of n levels of stones.
-    The first level has n stones.
-    The number of stones in the next level is:
-        - the next odd number if n is odd.
-        - the next even number if n is even.
-    Return the number of stones in each level in a list, where element at index
-    i represents the number of stones in the level (i+1).
-
-    Examples:
-    >>> make_a_pile(3)
-    [3, 5, 7]
+    You will be given a string of words separated by commas or spaces. Your task is
+    to split the string into words and return an array of the words.
+    
+    For example:
+    words_string("Hi, my name is John") == ["Hi", "my", "name", "is", "John"]
+    words_string("One, two, three, four, five, six") == ["One", "two", "three", "four", "five", "six"]
     """
