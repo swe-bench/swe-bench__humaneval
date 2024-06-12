5df74c4548f16475f35641dd9ad881f1dd5ed275
swe-bench <>
2024-06-12 16:29:59 -0400

    Task instance HumanEval/37

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 
 
-def fizz_buzz(n: int):
-    """Return the number of times the digit 7 appears in integers less than n which are divisible by 11 or 13.
-    >>> fizz_buzz(50)
-    0
-    >>> fizz_buzz(78)
-    2
-    >>> fizz_buzz(79)
-    3
+def sort_even(l: list):
+    """This function takes a list l and returns a list l' such that
+    l' is identical to l in the odd indicies, while its values at the even indicies are equal
+    to the values of the even indicies of l, but sorted.
+    >>> sort_even([1, 2, 3])
+    [1, 2, 3]
+    >>> sort_even([5, 6, 3, 4])
+    [3, 6, 5, 4]
     """
