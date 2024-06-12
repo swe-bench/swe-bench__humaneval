cf9ec629a602fcc06a762e54db419fd4b0e0207c
swe-bench <>
2024-06-12 16:30:07 -0400

    Task instance HumanEval/155

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
 
-def cycpattern_check(a , b):
-    """You are given 2 words. You need to return True if the second word or any of its rotations is a substring in the first word
-    cycpattern_check("abcd","abd") => False
-    cycpattern_check("hello","ell") => True
-    cycpattern_check("whassup","psus") => False
-    cycpattern_check("abab","baa") => True
-    cycpattern_check("efef","eeff") => False
-    cycpattern_check("himenss","simen") => True
+def even_odd_count(num):
+    """Given an integer. return a tuple that has the number of even and odd digits respectively.
 
+     Example:
+        even_odd_count(-12) ==> (1, 1)
+        even_odd_count(123) ==> (1, 2)
     """
