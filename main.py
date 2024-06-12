d561f54fc1cf85321206ad37a938955da26ef990
swe-bench <>
2024-06-12 16:30:04 -0400

    Task instance HumanEval/107

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
 
-def f(n):
-    """ Implement the function f that takes n as a parameter,
-    and returns a list of size n, such that the value of the element at index i is the factorial of i if i is even
-    or the sum of numbers from 1 to i otherwise.
-    i starts from 1.
-    the factorial of i is the multiplication of the numbers from 1 to i (1 * 2 * ... * i).
-    Example:
-    f(5) == [1, 2, 6, 24, 15]
+def even_odd_palindrome(n):
+    """
+    Given a positive integer n, return a tuple that has the number of even and odd
+    integer palindromes that fall within the range(1, n), inclusive.
+
+    Example 1:
+
+        Input: 3
+        Output: (1, 2)
+        Explanation:
+        Integer palindrome are 1, 2, 3. one of them is even, and two of them are odd.
+
+    Example 2:
+
+        Input: 12
+        Output: (4, 6)
+        Explanation:
+        Integer palindrome are 1, 2, 3, 4, 5, 6, 7, 8, 9, 11. four of them are even, and 6 of them are odd.
+
+    Note:
+        1. 1 <= n <= 10^3
+        2. returned tuple has the number of even and odd integer palindromes respectively.
     """
--- test.py
+++
@@ -1,8 +0,0 @@
-from main import f
-def check(candidate):
-
-    assert candidate(5) == [1, 2, 6, 24, 15]
-    assert candidate(7) == [1, 2, 6, 24, 15, 720, 28]
-    assert candidate(1) == [1]
-    assert candidate(3) == [1, 2, 6]
-check(f)
