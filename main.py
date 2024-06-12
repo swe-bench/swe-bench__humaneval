1bfd7b9974791a167c75a4cda5eb3a3196d8b508
swe-bench <>
2024-06-12 16:30:02 -0400

    Task instance HumanEval/88

--- main.py
+++ main.py
@@ -1,21 +1,16 @@
 
-def get_row(lst, x):
+def sort_array(array):
     """
-    You are given a 2 dimensional data, as a nested lists,
-    which is similar to matrix, however, unlike matrices,
-    each row may contain a different number of columns.
-    Given lst, and integer x, find integers x in the list,
-    and return list of tuples, [(x1, y1), (x2, y2) ...] such that
-    each tuple is a coordinate - (row, columns), starting with 0.
-    Sort coordinates initially by rows in ascending order.
-    Also, sort coordinates of the row by columns in descending order.
-    
+    Given an array of non-negative integers, return a copy of the given array after sorting,
+    you will sort the given array in ascending order if the sum( first index value, last index value) is odd,
+    or sort it in descending order if the sum( first index value, last index value) is even.
+
+    Note:
+    * don't change the given array.
+
     Examples:
-    get_row([
-      [1,2,3,4,5,6],
-      [1,2,3,4,1,6],
-      [1,2,3,4,5,1]
-    ], 1) == [(0, 0), (1, 4), (1, 0), (2, 5), (2, 0)]
-    get_row([], 1) == []
-    get_row([[], [1], [1, 2, 3]], 3) == [(2, 2)]
+    * sort_array([]) => []
+    * sort_array([5]) => [5]
+    * sort_array([2, 4, 3, 0, 1, 5]) => [0, 1, 2, 3, 4, 5]
+    * sort_array([2, 4, 3, 0, 1, 5, 6]) => [6, 5, 4, 3, 2, 1, 0]
     """
